9d68455652ecff4b7a884d5774b65760c7336293
qja1998 <62369107+qja1998@users.noreply.github.com>
2023-12-21 15:36:02 +0900

    [Silver V] Title: 소트인사이드, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,13 +1,12 @@
 import sys
-from collections import defaultdict
 input = sys.stdin.readline
 
-num = input()
-cnt_dict = defaultdict(int)
+num = input().strip()
+cnt_dict = {str(n):0 for n in range(9, -1, -1)}
 for i in num:
     cnt_dict[i] += 1
 ans = ''
-for i in sorted(cnt_dict, reverse=True):
+for i in cnt_dict:
     for _ in range(cnt_dict[i]):
         ans += i
 
