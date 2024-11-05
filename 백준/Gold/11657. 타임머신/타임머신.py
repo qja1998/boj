a867e3354bc611faeb04faece776550227117f6b
Germanus <62369107+qja1998@users.noreply.github.com>
2024-11-06 01:05:21 +0900

    [Gold IV] Title: 타임머신, Time: 204 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -1,27 +1,29 @@
 from collections import defaultdict
 N, M = map(int, input().split())
 
+INF = float('inf')
+
 bus_graph = defaultdict(list)
 for _ in range(M):
     a, b, c = map(int, input().split())
     bus_graph[a].append((b, c))
 
 def bellman(start, graph):
-    dist = [float('inf')] * (N + 1)
+    dist = [INF] * (N + 1)
     dist[start] = 0
 
     for _ in range(N-1):
         for c_node in graph:
             for n_node, cost in graph[c_node]:
 
-                if dist[c_node] != float('inf') and dist[n_node] > dist[c_node] + cost:
+                if dist[c_node] != INF and dist[n_node] > dist[c_node] + cost:
                     dist[n_node] = dist[c_node] + cost
 
     for c_node in graph:
         for n_node, cost in graph[c_node]:
 
             # 또 갱신되는 경우(음수 사이클)
-            if dist[c_node] != float('inf') and dist[n_node] > dist[c_node] + cost:
+            if dist[c_node] != INF and dist[n_node] > dist[c_node] + cost:
                 return False
 
     return dist[2:]
@@ -29,7 +31,7 @@ def bellman(start, graph):
 result = bellman(1, bus_graph)
 if result:
     for d in result:
-        if d == float('inf'):
+        if d == INF:
             print(-1)
         else:
             print(d)
