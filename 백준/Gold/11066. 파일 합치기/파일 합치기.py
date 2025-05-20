0c58cbff7583561e6e96fdcdcde4d0228cf5bc64
Germanus <62369107+qja1998@users.noreply.github.com>
2025-05-20 22:44:45 +0900

    [Gold III] Title: 파일 합치기, Time: 2140 ms, Memory: 119668 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -1,24 +1,25 @@
-import heapq
+# dp[i][j] = i ~ j 까지 합쳤을 때 비용이 가장 작은 경우
+#          = min(원래 i ~ j까지의 최소 비용, k를 거쳐 합칠 때 최소 비용(i < k < j))
+INF = float("inf")
 T = int(input())
 
 for _ in range(T):
     K = int(input())
-
-    res = 0
     files = list(map(int, input().split()))
-    
-    dp = [[0] * K for _ in range(K)]
-    sum_list = [0] * (K + 1)
 
-    # 누적합
+    dp = [[0] * K for _ in range(K)]
+
+    sum_list = [0] * (K + 1)
     for i in range(K):
         sum_list[i + 1] = sum_list[i] + files[i]
-    
+
     for i in range(1, K):
         for j in range(i, K):
-            dp[j - i][j] = float('inf')
+            dp[j - i][j] = INF
             for k in range(j - i, j):
                 dp[j - i][j] = min(dp[j - i][j], dp[j - i][k] + dp[k + 1][j])
+            # 누적 합으로 빠르게 구간 합 계산
             dp[j - i][j] += sum_list[j + 1] - sum_list[j - i]
-    
+
+    # print(*dp, sep="\n")
     print(dp[0][K - 1])
