40512ee6862301a7ffc1090cdd6ee0ff9c49e60c
Germanus <62369107+qja1998@users.noreply.github.com>
2025-06-14 13:05:06 +0900

    [Platinum V] Title: 볼록 껍질, Time: 372 ms, Memory: 121864 KB -BaekjoonHub

--- 백준/Platinum/1708. 볼록 껍질/볼록 껍질.py
+++ 백준/Platinum/1708. 볼록 껍질/볼록 껍질.py
@@ -1,32 +1,25 @@
-def inclination(p1, p2):
-    return p2[0] - p1[0], p2[1] - p1[1]
- 
-def ccw(p1, p2, p3):
-    v, u = inclination(p1, p2), inclination(p2, p3)
-    if v[0] * u[1] > v[1] * u[0]:
-        return True
-    return False
-     
-def convex_hull(positions):
-    convex = list()
-    for p3 in positions:
-        while len(convex) >= 2:
-            p1, p2 = convex[-2], convex[-1]
-            if ccw(p1, p2, p3):
-                break
-            convex.pop()
-        convex.append(p3)
-     
-    return len(convex)
- 
-n, answer = int(input()), -2
-positions = list()
-for i in range(n):
-    positions.append(list(map(int, input().split())))
-     
-positions = sorted(positions, key=lambda pos:(pos[0], pos[1]))
-answer += convex_hull(positions)
- 
-positions.reverse()
-answer += convex_hull(positions)
-print(answer)
+def diff(a, b):
+    return b[0] - a[0], b[1] - a[1]
+
+def is_ccw(a, b, c):
+    v1 = diff(a, b)
+    v2 = diff(b, c)
+    return v1[0] * v2[1] > v1[1] * v2[0]
+
+def hull(points):
+    stack = []
+    for pt in points:
+        while len(stack) >= 2 and not is_ccw(stack[-2], stack[-1], pt):
+            stack.pop()
+        stack.append(pt)
+    return len(stack)
+
+n = int(input())
+pts = [tuple(map(int, input().split())) for _ in range(n)]
+pts.sort()
+
+res = -2
+res += hull(pts)
+res += hull(pts[::-1])
+
+print(res)
